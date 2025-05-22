39c2dfa04f0694997a5a587c8d9e4c5bf44690d1
Won0001 <smjwnaya1102@gmail.com>
2025-05-22 22:45:29 +0900

    feat : 맵 구성 및 꾸미기
    - 맵 구성 및 꾸미기

---
+++ Assets/Scripts/Pads/FakePad.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class FakePad : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
